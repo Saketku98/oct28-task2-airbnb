b0d006ed0a422edf01adb5461ff4868d7acc8a3a
Saketku98 <88224770+Saketku98@users.noreply.github.com>
2022-10-29 21:02:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+ import './App.css';
+ import pic from './airbnblogo.webp';
+
+function App() {
+  return (
+    <div className = "box">
+      <div className= "item">
+   <h1>The Evolution of Airbnb's Frontend</h1>
+   <p>@spikebrehm</p>
+   <img src={pic} alt="airbnb" />
+   </div>
+   </div>
+  )
+}
+
+export default App;
